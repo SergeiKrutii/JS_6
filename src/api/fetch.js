b1d66e3cc0dcff22854087b62eb2884169df40fa
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-01-29 02:42:25 +0200

    rework with class#1

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,16 +1,56 @@
 const axios = require('axios').default;
-const BASE_URL =
-  'https://pixabay.com/api/?key=33125565-bcbdb194cdd5c3277aaf5f84a&';
-const params = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=12';
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '?key=33125565-bcbdb194cdd5c3277aaf5f84a&';
+const params =
+'image_type=photo&orientation=horizontal&safesearch=true&page=5&per_page=12';
 
-async function getImages(inputValue) {
-  try {
-    const response = await axios.get(`${BASE_URL}q=${inputValue}&${params}`);
-    const data = response.data.hits;
-    return data;
-  } catch (error) {
-    console.error(error);
+export default class NewImageService {
+  constructor() {
+    this.searchQuery = 'cat';
+    this.page = 1;
+    
+  }
+  
+  async fetchImages() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}${API_KEY}q=${this.searchQuery}&${params}`
+      );
+      const data = response.data.hits;
+      this.incrementPage();
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
+// async function fetchImages(inputValue) {
+  //   try {
+//     const response = await axios.get(
+//       `${BASE_URL}${API_KEY}q=${inputValue}&${params}`
+//     );
+//     const data = response.data.hits;
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-export { getImages };
+// export { getImages };
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { refs } from './api/refs';
-import { getImages } from './api/fetch';
 import { makeMarcup } from './api/markup';
+import NewImageService from './api/fetch';
 
 refs.form.addEventListener('submit', onImageSearch);
 refs.button.addEventListener('click', onButtonClick);
+const NewImageService = new NewImageService();
 
-let paginatinon = 1;
+refs.button.setAttribute('hidden', true);
 
-console.dir(refs.button);
-
-refs.button.setAttribute('hidden', true)
 async function onImageSearch(e) {
-    e.preventDefault();
-    const inputValue = refs.form.elements.searchQuery.value;
-    const pictures = await getImages(inputValue);
-    const markup = makeMarcup(pictures);
-    if (markup.length === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    refs.div.insertAdjacentHTML('beforeend', markup);
-    refs.button.removeAttribute('hidden');
-    
+  e.preventDefault();
+
+  console.log(NewImageService)
+  // NewImageService.query = e.currentTarget.elements.searchQuery.value;
+  // if (NewImageService.query === '') {
+  //   return alert('clear')
+  // }
+  // const images = await NewImageService.fetchImages();
+  // const pictures = NewImageService.fetchImages();
+  // const markup = makeMarcup(pictures);
+  // console.log(pictures);
+  // if (markup.length === 0) {
+  //   Notify.failure(
+  //     'Sorry, there are no images matching your search query. Please try again.'
+  //   );
+  // }
+  // refs.div.insertAdjacentHTML('beforeend', markup);
+  refs.button.removeAttribute('hidden');
 }
 
 function onButtonClick() {
-    paginatinon += 1;
+  paginatinon += 1;
 }
