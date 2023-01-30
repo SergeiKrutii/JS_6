91ea2681708a2ea8e32b32f195dfdf23a0f9db95
unknown <sergei.krutii01@gmail.com>
2023-01-30 18:37:24 +0300

    rework with class#5

--- src/index.js
+++ src/index.js
@@ -11,12 +11,16 @@ refs.button.setAttribute('hidden', true);
 
 async function onImageSearch(e) {
   e.preventDefault();
+  refs.button.setAttribute('hidden', true);
   imageService.query = e.currentTarget.elements.searchQuery.value;
-
+  
   imageService.resetPage();
+  (() => {
+    refs.div.innerHTML = '';
+  })()
 
-  const pictures = await imageService.fetchImages();
-  // clearGallery();
+  const { hits: pictures, totalHits } = await imageService.fetchImages();
+  console.log(pictures);
 
   if (pictures.length === 0) {
     Notify.failure(
@@ -24,21 +28,33 @@ async function onImageSearch(e) {
     );
     return;
   }
-  
+  checkImageAmount(pictures);
+
+  Notify.info(`Hooray! We found ${totalHits} images.`);
+
   appendMarcup(pictures);
+  console.log('34')
   refs.button.removeAttribute('hidden');
 }
 
 async function onLoadMore() {
   imageService.incrementPage();
-  const pictures = await imageService.fetchImages();
-  console.log(pictures);
+  const { hits: pictures } = await imageService.fetchImages();
+  checkImageAmount();
   appendMarcup(pictures);
 }
-// function clearGallery() {
-//   refs.div.insertAdjacentHTML = '';
-// }
-
 function appendMarcup(pictures) {
   refs.div.insertAdjacentHTML('beforeend', makeMarcup(pictures));
+}
+function checkImageAmount(pictures) {
+  if (pictures.length < 40) {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.button.setAttribute('hidden', true);
+    appendMarcup(pictures);
+    console.log('check')
+    // return;
+  }
+  return;
 }
